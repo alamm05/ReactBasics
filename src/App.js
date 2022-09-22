c9ef863a001a0e5174558a2d06b4dabb9831094e
Rashid Alam <alamm2412@gmail.com>
2022-09-22 20:58:06 +0530

    :construction:contextAPI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Ue1 from './Components/Ue1';
 import Ue2 from './Components/Ue2'
 import Infinite from './Components/Infinite';
 import Ue3 from './Components/Ue3';
+import {useState} from 'react';
+import context from './Components/Context';
+import Navbar from './Components/Navbar';
+import Parent1 from './Components/Parent1';
+import Parent2 from './Components/Parent2';
+
+
+
 function App() {
+  const [theme,setTheme] =useState(false);
+  const [count, setCount] =useState(10);
   return (
-    // <Us></Us>
-      //  <Ue1></Ue1>
-      // <Ue2></Ue2>
-      // <Infinite></Infinite>
+    <context.Provider value={{theme,count}}>
+      <button onClick={()=>setTheme(!theme)}>Change</button>
+    
+      {/* <Us></Us>
+      <Infinite></Infinite>
       <Ue3></Ue3>
+      <Ue1></Ue1>
+      <Ue2></Ue2> */}
+      <Navbar/>
+      <Parent1/>
+      <Parent2/>
+      </context.Provider>
   );
 }
 
---
+++ src/Child.js
@@ -0,0 +1,8 @@
+import React,{useContext} from "react";
+
+function Child({theme}){
+    return <div className={theme ? "dark" : "light"}>Child</div>
+
+}
+
+export default Child;
